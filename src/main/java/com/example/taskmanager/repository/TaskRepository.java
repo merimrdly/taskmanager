691438de75637d1650581fcb2f7c3cf7afb130dd
irem yıldırım <iremyildirim45500@icloud.com>
2026-04-22 11:47:48 +0300

    Repository Class is created for Task Entity

---
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.example.taskmanager.repository;
+
+import org.example.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
