6d65d1ca8dbd73cef149cc72103f950f90acd63b
irem yıldırım <iremyildirim45500@icloud.com>
2026-05-06 12:15:19 +0300

    Global.ExceptionHandler is added

--- src/main/java/com/example/taskmanager/controller/HelloController.java
+++ src/main/java/com/example/taskmanager/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.example.taskmanager.controller;
+package com.example.taskmanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
